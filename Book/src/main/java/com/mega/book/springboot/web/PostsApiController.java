8b1b541e4194f69503974cdd8854351eadc13570
HyunKim3 <wadsijlk1233@gmail.com>
2023-03-17 13:52:28 +0900

    1.글 관련 기능 구현 2.템플릿 머스타치 세팅 3.Audting 기능 구현

--- Book/src/main/java/com/mega/book/springboot/web/PostsApiController.java
+++ Book/src/main/java/com/mega/book/springboot/web/PostsApiController.java
@@ -1,12 +1,16 @@
 package com.mega.book.springboot.web;
 
 
+import com.mega.book.springboot.domain.posts.Posts;
 import com.mega.book.springboot.service.PostsService;
+import com.mega.book.springboot.web.dto.PostsResponseDto;
 import com.mega.book.springboot.web.dto.PostsSaveRequestDto;
+import com.mega.book.springboot.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.LocalDateTime;
+
 
 @RequiredArgsConstructor
 @RestController
@@ -18,5 +22,16 @@ public class PostsApiController {
     public Long save(@RequestBody PostsSaveRequestDto requestDto){
         return postsService.save(requestDto);
     }
+
+    @PutMapping("/api/v1/posts/{id}")
+    public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto){
+        return postsService.update(id, requestDto);
+    }
+
+    @GetMapping("/api/v1/posts/{id}")
+    public PostsResponseDto findById(@PathVariable Long id){
+        return postsService.findById(id);
+    }
+
 
 }
